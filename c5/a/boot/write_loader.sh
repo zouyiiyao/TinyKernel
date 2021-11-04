a2ee702d1984ceac2902a4ad4dd20b9bd65d80f7
zouyiiyao <zouyiiyao@163.com>
2021-11-03 20:58:14 -0700

    loader中增加获取物理内存代码

---
+++ c5/a/boot/write_loader.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+dd if=/home/zouyi/Desktop/os/code/c5/a/boot/loader.bin of=/home/zouyi/Desktop/os/bochs/bin/hd60M.img bs=512 count=4 seek=2 conv=notrunc
---
+++ c5/a/boot/write_mbr.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+dd if=/home/zouyi/Desktop/os/code/c5/a/boot/mbr.bin of=/home/zouyi/Desktop/os/bochs/bin/hd60M.img bs=512 count=1 conv=notrunc
