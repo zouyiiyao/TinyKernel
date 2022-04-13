4525384a1a1e117728c4c6c38fe257c7b5a5d28b
zouyiiyao <zouyiiyao@163.com>
2022-04-12 22:14:37 -0700

    实现硬盘驱动

---
+++ c13/d/boot/write_loader.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+dd if=loader.bin of=/home/zouyi/Desktop/os/bochs/bin/hd60M.img bs=512 count=4 seek=2 conv=notrunc
---
+++ c13/d/kernel/init.c
@@ -0,0 +1,25 @@
+#include "init.h"
+#include "print.h"
+#include "interrupt.h"
+#include "../device/timer.h"    // 用相对路径演示头文件包含
+#include "memory.h"
+#include "thread.h"
+#include "console.h"
+#include "keyboard.h"
+#include "tss.h"
+#include "syscall_init.h"
+#include "ide.h"
+
+/* 负责初始化所有模块 */
+void init_all() {
+	put_str("init_all\n");
+	idt_init();         // 初始化中断
+	timer_init();       // 初始化PIT，含时钟中断处理程序注册
+	mem_init();         // 初始化内存管理系统
+	thread_init();      // 初始化线程
+	console_init();     // 初始化控制台，最好在开中断前
+	keyboard_init();    // 初始化键盘
+	tss_init();         // 初始化tss
+	syscall_init();     // 初始化系统调用
+	ide_init();         // 初始化硬盘
+}
